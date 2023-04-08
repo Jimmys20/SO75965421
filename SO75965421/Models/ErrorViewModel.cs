f78850940f8b7e0566909583b254652d36f74cc6
dimitris <maragkosd@hotmail.com>
2023-04-08 17:22:57 +0300

    commit

---
+++ SO75965421/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SO75965421.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
